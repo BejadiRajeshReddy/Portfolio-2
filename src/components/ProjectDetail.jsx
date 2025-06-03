2eeb3f6b6d196564b40aa23a56f481254e804dc6
Rajesh Reddy Bejadi <64033035+BejadiRajeshReddy@users.noreply.github.com>
2025-06-04 01:31:29 +0530

    Updated ProjectDetail.jsx

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -122,7 +122,7 @@ const ProjectDetails = () => {
   }
 
   return (
-    <div className="min-h-screen  px-[2%] sm:px-0 relative overflow-hidden p-28">
+    <div className="min-h-screen  px-[2%] sm:px-0 relative overflow-hidden">
       {/* Background animations */}
       <div className="fixed inset-0">
         <div className="absolute -inset-[10px] opacity-20">
